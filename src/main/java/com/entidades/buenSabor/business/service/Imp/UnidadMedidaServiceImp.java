0546579ef6ac0ba91c2cdeb6fa64b6bf0dfdb624
vGoen <francoezequielbarroso@gmail.com>
2024-09-11 16:40:56 -0300

    fix unidad medida

--- src/main/java/com/entidades/buenSabor/business/service/Imp/UnidadMedidaServiceImp.java
+++ src/main/java/com/entidades/buenSabor/business/service/Imp/UnidadMedidaServiceImp.java
@@ -2,9 +2,26 @@ package com.entidades.buenSabor.business.service.Imp;
 
 import com.entidades.buenSabor.business.service.Base.BaseServiceImp;
 import com.entidades.buenSabor.business.service.UnidadMedidaService;
+import com.entidades.buenSabor.domain.entities.Articulo;
 import com.entidades.buenSabor.domain.entities.UnidadMedida;
+import com.entidades.buenSabor.repositories.ArticuloRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Set;
 
 @Service
 public class UnidadMedidaServiceImp extends BaseServiceImp<UnidadMedida, Long> implements UnidadMedidaService {
+    @Autowired
+    private ArticuloRepository articuloRepository;
+    @Override
+    public void deleteById(Long id) {
+        Set<Articulo> articulos = this.articuloRepository.findByEliminadoFalse();
+        for(Articulo articulo : articulos) {
+            if(Objects.equals(articulo.getUnidadMedida().getId(), id))
+                throw new RuntimeException("No se puede eliminar la unidad de medida, esta presente en un articulo");
+        }
+        super.deleteById(id);
+    }
 }
--- src/main/java/com/entidades/buenSabor/repositories/ArticuloRepository.java
+++ src/main/java/com/entidades/buenSabor/repositories/ArticuloRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ArticuloRepository  extends BaseRepository<Articulo, Long> {
+    Set<Articulo> findByEliminadoFalse();
 }
